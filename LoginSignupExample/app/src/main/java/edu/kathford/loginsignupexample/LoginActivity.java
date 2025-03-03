4053c69f628ccd439d1bfeebe2438376cf777055
Dinesh Maharjan <www.maharjandinesh@gmail.com>
2025-03-03 08:16:58 +0545

    Login examPLE

--- LoginSignupExample/app/src/main/java/edu/kathford/loginsignupexample/LoginActivity.java
+++ LoginSignupExample/app/src/main/java/edu/kathford/loginsignupexample/LoginActivity.java
@@ -2,8 +2,11 @@ package edu.kathford.loginsignupexample;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,10 +22,64 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
-        usernameEditText= findViewById(R.id.usernameEditText);
-        passwordEditText=findViewById(R.id.passwordEditText);
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
         signInButton = findViewById(R.id.signInButton);
 
 
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameEditText.getText()
+                        .toString().trim();
+                String password = passwordEditText.getText()
+                        .toString().trim();
+
+//           validationOne(username,password);
+                validationTwo(username, password);
+
+
+                Log.d("Username", username);
+                Log.d("Password", password);
+//                Toast.makeText(LoginActivity.this,
+//                        "Sign-in button clicked",
+//                        Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
     }
+
+
+    private void validationOne(String username,
+                               String password) {
+
+        if (username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(LoginActivity.this,
+                    "Invalid username or password",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(LoginActivity.this,
+                    "Login success",
+                    Toast.LENGTH_LONG).show();
+            ;
+        }
+    }
+
+    private void validationTwo(String username,
+                               String password) {
+        if (username.isEmpty()) {
+            usernameEditText.setError("Invalid username");
+        } else {
+            if (password.isEmpty()) {
+                passwordEditText.setError("Invalid password");
+            } else {
+                Toast.makeText(LoginActivity.this,
+                        "Login Successful", Toast.LENGTH_LONG).show();
+                ;
+            }
+        }
+    }
+
 }
